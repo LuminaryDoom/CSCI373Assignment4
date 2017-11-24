9d68878819e26cee8de486ea8fefac4cd53a0a42
Bryant Ramirez <bryantramirez@ac.c4q.nyc>
2017-11-24 16:15:38 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Assignment4DataStruct.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment4DataStruct.dir/main.cpp.o"
+  "Assignment4DataStruct.pdb"
+  "Assignment4DataStruct.exe"
+  "libAssignment4DataStruct.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment4DataStruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
